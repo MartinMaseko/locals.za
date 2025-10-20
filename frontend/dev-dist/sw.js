f61843cdd815b2d9747b9a6860d77f53143b4691
martin_maseko <km.masekoccs@gmail.com>
2025-10-20 11:48:03 +0200

    product recommendation fix

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ljafnr860lg"
+    "revision": "0.nhu37tkqbng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
