d6ed6183f70c4df02b7d59f46599582ea1a430ba
martin_maseko <km.masekoccs@gmail.com>
2025-11-10 19:25:42 +0200

    PWA updates

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.h8ih04ljob"
+    "revision": "0.26r52c7p8vg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
