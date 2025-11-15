5bfce6676ae6b80eca11cc3a9fef48f7d4654f62
martin_maseko <km.masekoccs@gmail.com>
2025-11-15 22:25:21 +0200

    payfast updates 003

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hpk7vvih3ig"
+    "revision": "0.27s9ur43t18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
