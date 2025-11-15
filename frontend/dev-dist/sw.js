a9708348426603db003103892d33575305d00c3f
martin_maseko <km.masekoccs@gmail.com>
2025-11-15 17:56:43 +0200

    Navbar hieght adjustment

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.61q0k8q2le8"
+    "revision": "0.on6uu8vemd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
