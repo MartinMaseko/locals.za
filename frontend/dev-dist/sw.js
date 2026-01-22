b368324f409e956796510d1493bf11b0f1bf5b64
martin_maseko <km.masekoccs@gmail.com>
2026-01-22 18:19:14 +0200

    Homepage typescript deployment error resolved

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.c2ljjmg202g"
+    "revision": "0.ipsp2mt5o28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
