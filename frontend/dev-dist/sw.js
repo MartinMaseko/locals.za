4b5ddc265702d58f74f58eabd55f5e01516d52f6
martin_maseko <km.masekoccs@gmail.com>
2026-03-24 11:18:26 +0200

    admin status filter removal

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hgkrk7jmqt8"
+    "revision": "0.1f9gspg6o3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
